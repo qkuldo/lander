2b013937fe799a8fa30e855aae96bb3ee4b70d3b
qkuldo <teoa52269@gmail.com>
2025-07-08 13:08:28 +0800

    initial commit

---
+++ game.py
@@ -0,0 +1,9 @@
+import pygame as pg
+import sys
+import modules
+pg.init()
+screen = pg.display.set_mode((1000,1000),pg.FULLSCREEN)
+clock = pg.time.Clock()
+TITLESCENE = 0
+GAMESCENE = 1
+current_scene = 1
