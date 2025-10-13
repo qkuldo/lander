c8db29774121771fb81b2d7c4e065f80838ce233
qkuldo <teoa52269@gmail.com>
2025-10-13 17:37:50 +0800

    Removed bug that added black background to images
    Signed-off-by: qkuldo <teoa52269@gmail.com>

--- modules/spritesheet.py
+++ modules/spritesheet.py
@@ -7,7 +7,7 @@ class Spritesheet:
 		self.height = height
 	def load_frame(self,frame):
 		# Load and return a specific frame from the spritesheet.
-		image = pg.Surface((self.width,self.height)).convert_alpha()
+		image = pg.Surface((self.width,self.height))
 		image.blit(self.sheet,(0,0),((frame*self.width),0,self.width,self.height))
 		image.set_colorkey((0,0,0))
 		return image
